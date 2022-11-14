74ea18191bc04de8e543935b716b0fe70d2d7d6e
Mickey <mickey@Mihits-MacBook-Pro.local>
2022-11-14 17:28:22 +0530

    class to function component

--- src/index.ts
+++ src/index.ts
@@ -4,24 +4,17 @@ import { getIsBrowser } from "./utils/get-is-browser";
 import { cleanUpLocalStorage } from "./utils/clean-local-storage";
 import { getData } from "./utils/get-data";
 
-export default class SDK {
-  isClicked = false;
-  appId = "";
-  enableErrorLogging = false;
-  url = "";
-  constructor({ appId = "", enableErrorLogging = false, url = URL }) {
-    if (!getIsBrowser()) {
-      throw new Error("window object not defined");
-    }
-    if (!appId) {
-      throw new Error("appId not found");
-    }
-    this.appId = appId;
-    this.enableErrorLogging = enableErrorLogging;
-    this.url = url;
+const SDK = ({ appId = "", enableErrorLogging = false, url = URL }) => {
+  if (!getIsBrowser()) {
+    throw new Error("window object not defined");
+  }
+  if (!appId) {
+    throw new Error("appId not found");
   }
 
-  static getTokenFromQueryParams = () => {
+  let isClicked = false;
+
+  const getToken = () => {
     if (!getIsBrowser()) {
       return null;
     }
@@ -29,40 +22,111 @@ export default class SDK {
     return params.get(TOKEN);
   };
 
-  static getState = () => {
+  const getState = () => {
     if (!getIsBrowser()) {
       return null;
     }
     return localStorage.getItem(STATE_LOCAL_STORAGE_KEY);
   };
 
-  createGetIntentOnClick = ({ redirectionURL, orderId }) => {
+  const getIntent = ({ redirectionURL, orderId }) => {
     if (!getIsBrowser()) {
       return null;
     }
     return async () => {
-      if (this.isClicked) {
+      if (isClicked) {
         return null;
       }
-      this.isClicked = true;
+      isClicked = true;
       cleanUpLocalStorage();
       const state = makeState(6);
       localStorage.setItem(STATE_LOCAL_STORAGE_KEY, state);
 
       const data = await getData({
-        url: this.url,
+        url,
         redirectionURL,
         state,
         orderId,
-        enableErrorLogging: this.enableErrorLogging,
-        appId: this.appId,
+        enableErrorLogging,
+        appId,
       });
       const intent = data && data.intent;
       if (intent) {
         location.replace(intent);
       } else {
-        this.isClicked = false;
+        isClicked = false;
       }
     };
   };
-}
+
+  return {
+    getToken,
+    getState,
+    getIntent,
+  };
+};
+
+export default SDK;
+
+// export default class SDK {
+//   isClicked = false;
+//   appId = "";
+//   enableErrorLogging = false;
+//   url = "";
+//   constructor({ appId = "", enableErrorLogging = false, url = URL }) {
+//     if (!getIsBrowser()) {
+//       throw new Error("window object not defined");
+//     }
+//     if (!appId) {
+//       throw new Error("appId not found");
+//     }
+//     this.appId = appId;
+//     this.enableErrorLogging = enableErrorLogging;
+//     this.url = url;
+//   }
+
+//   static getTokenFromQueryParams = () => {
+//     if (!getIsBrowser()) {
+//       return null;
+//     }
+//     const params = new URLSearchParams(window.location.search);
+//     return params.get(TOKEN);
+//   };
+
+//   static getState = () => {
+//     if (!getIsBrowser()) {
+//       return null;
+//     }
+//     return localStorage.getItem(STATE_LOCAL_STORAGE_KEY);
+//   };
+
+//   createGetIntentOnClick = ({ redirectionURL, orderId }) => {
+//     if (!getIsBrowser()) {
+//       return null;
+//     }
+//     return async () => {
+//       if (this.isClicked) {
+//         return null;
+//       }
+//       this.isClicked = true;
+//       cleanUpLocalStorage();
+//       const state = makeState(6);
+//       localStorage.setItem(STATE_LOCAL_STORAGE_KEY, state);
+
+//       const data = await getData({
+//         url: this.url,
+//         redirectionURL,
+//         state,
+//         orderId,
+//         enableErrorLogging: this.enableErrorLogging,
+//         appId: this.appId,
+//       });
+//       const intent = data && data.intent;
+//       if (intent) {
+//         location.replace(intent);
+//       } else {
+//         this.isClicked = false;
+//       }
+//     };
+//   };
+// }
