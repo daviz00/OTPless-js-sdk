af5dcada29dfb8a61ae280997270f5215791965e
Ashwin Garg <ashwingarg@Ashwins-MacBook-Pro.local>
2022-11-21 21:53:52 +0530

    otpless-js-sdk: build fix

--- src/index.ts
+++ src/index.ts
@@ -34,7 +34,7 @@ const OTPlessSdk = ({
     return localStorage.getItem(STATE_LOCAL_STORAGE_KEY);
   };
 
-  const getIntent = ({ redirectionURL, orderId }: GetIntentParams = {}) => {
+  const createGetIntentOnClick = ({ redirectionURL, orderId }: GetIntentParams = {}) => {
     if (!getIsBrowser()) {
       return null;
     }
@@ -63,71 +63,9 @@ const OTPlessSdk = ({
   return {
     getToken,
     getState,
-    getIntent,
+    createGetIntentOnClick,
   };
 };
 
 export default OTPlessSdk;
 
-// export default class SDK {
-//   isClicked = false;
-//   appId = "";
-//   enableErrorLogging = false;
-//   url = "";
-//   constructor({ appId = "", enableErrorLogging = false, url = URL }) {
-//     if (!getIsBrowser()) {
-//       throw new Error("window object not defined");
-//     }
-//     if (!appId) {
-//       throw new Error("appId not found");
-//     }
-//     this.appId = appId;
-//     this.enableErrorLogging = enableErrorLogging;
-//     this.url = url;
-//   }
-
-//   static getTokenFromQueryParams = () => {
-//     if (!getIsBrowser()) {
-//       return null;
-//     }
-//     const params = new URLSearchParams(window.location.search);
-//     return params.get(TOKEN);
-//   };
-
-//   static getState = () => {
-//     if (!getIsBrowser()) {
-//       return null;
-//     }
-//     return localStorage.getItem(STATE_LOCAL_STORAGE_KEY);
-//   };
-
-//   createGetIntentOnClick = ({ redirectionURL, orderId }) => {
-//     if (!getIsBrowser()) {
-//       return null;
-//     }
-//     return async () => {
-//       if (this.isClicked) {
-//         return null;
-//       }
-//       this.isClicked = true;
-//       cleanUpLocalStorage();
-//       const state = makeState(6);
-//       localStorage.setItem(STATE_LOCAL_STORAGE_KEY, state);
-
-//       const data = await getData({
-//         url: this.url,
-//         redirectionURL,
-//         state,
-//         orderId,
-//         enableErrorLogging: this.enableErrorLogging,
-//         appId: this.appId,
-//       });
-//       const intent = data && data.intent;
-//       if (intent) {
-//         location.replace(intent);
-//       } else {
-//         this.isClicked = false;
-//       }
-//     };
-//   };
-// }
